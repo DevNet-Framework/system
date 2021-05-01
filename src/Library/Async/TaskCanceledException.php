fdacd5689ce6d0b4ed2d795da5e3839d923c0836
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-05-01 02:38:53 -0400

    add TaskCanceledException

---
+++ src/Library/Async/TaskCanceledException.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\System\Async;
+
+use Exception;
+
+class TaskCanceledException extends Exception
+{
+    
+}
