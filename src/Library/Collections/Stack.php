7da6f44a13641487d257dda0aa8e75552a0371e6
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-05-21 09:50:00 -0400

    update Stack Collection

--- src/Library/Collections/Stack.php
+++ src/Library/Collections/Stack.php
@@ -8,15 +8,75 @@
 
 namespace DevNet\System\Collections;
 
-class Stack extends ArrayList
+use DevNet\System\Type;
+use DevNet\System\Text\StringBuilder;
+use DevNet\System\Exceptions\TypeException;
+use DevNet\System\Exceptions\ErrorMessageExtension;
+
+class Stack implements IEnumerable
 {
-    public function peek()
+    use \DevNet\System\Extension\ExtensionTrait;
+
+    private array $Array = [];
+    private Type $GenericType;
+
+    public function __construct(string $valueType)
     {
-        return end($this->Array);
+        $this->GenericType = new Type(self::class, new Type($valueType));
+    }
+
+    public function push($value) : void
+    {
+        $index = $this->GenericType->validateArguments($value);
+        if ($index > 0)
+        {
+            $message = new StringBuilder();
+            $message->invalidArgumentType(get_class($this), 'push', $index, $this->GenericType->GenericTypeArgs[$index-1]->Name);
+            throw new TypeException($message->__toString());
+        }
+
+        $this->Array[$value];
     }
 
     public function pop()
     {
         return array_pop($this->Array);
+    }
+
+    public function peek()
+    {
+        return end($this->Array);
+    }
+
+    public function contains($item) : bool
+    {
+        return in_array($item, $this->Array);
+    }
+
+    public function remove($item) : void
+    {
+        if (isset($this->Array[$item])) {
+            unset($this->Array[$item]);
+        }
+    }
+
+    public function clear() : void
+    {
+        $this->Array = [];
+    }
+    
+    public function getIterator() : Enumerator
+    {
+        return new Enumerator($this->Array);
+    }
+
+    public function toArray() : array
+    {
+        return $this->Array;
+    }
+
+    public function getType() : Type
+    {
+        return $this->GenericType;
     }
 }
