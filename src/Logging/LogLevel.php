bff28975cc442d28727acd13d19f2959347d970c
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-10-28 01:23:41 -0400

    add LogLevel

---
+++ src/Logging/LogLevel.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\System\Logging;
+
+interface LogLevel
+{
+    /**
+     * Logs that are used for interactive investigation during development.
+     */
+    const Debug = 1;
+
+    /**
+     * Logs that track the general flow of the application.
+     */
+    const Notice = 2;
+
+    /**
+     * Logs that highlight the abnormal in the application flow, without stopping the execution.
+     */
+    const Warning = 3;
+
+    /**
+     * Logs that highlight when the current flow of execution is stopped due to a failure.
+     */
+    const Error = 4;
+
+    /**
+     * Logs that describe a system crash, or a catastrophic failure that requires immediate attention.
+     */
+    const Critical = 5;
+
+}
