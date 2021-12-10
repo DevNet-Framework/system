3bc04a97a0c6c8559620e3c34bd8d91ab553a007
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-12-10 01:55:06 -0500

    add IAwaitable interface

---
+++ lib/Async/IAwaitable.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\System\Async;
+
+interface IAwaitable
+{
+    /**
+     * return wait handle of type IAwaiter.
+     */
+    public function getAwaiter(): IAwaiter;
+}
