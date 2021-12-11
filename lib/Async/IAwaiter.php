4c41b9ee59f34e1d071b22dc4829250a9cb43528
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-12-11 16:58:13 -0500

    update IAwaiter interface add onCompleted method

--- lib/Async/IAwaiter.php
+++ lib/Async/IAwaiter.php
@@ -9,15 +9,24 @@
 
 namespace DevNet\System\Async;
 
+use Closure;
+
 interface IAwaiter
 {
+    /**
+     * Set the continuation handler to the property OnCompleted event.
+     * If the OnCompleted event is not null, it must be triggered only once after the wait handle is completed when the user calls the getResult method.
+     */
+    public function onCompleted(Closure $continuation): void;
+
+
     /**
      * Check if this wait handle is Completed (succeeded, canceled or failed)
      */
     public function IsCompleted(): bool;
     
     /**
-     * Wait this wait till is Completed then return the result.
+     * Wait this wait handle till is Completed then return the result.
      */
     public function getResult();
 }
