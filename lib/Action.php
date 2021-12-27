b28b2aa613f640dfa4f30117941ea73ad9eabd97
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-12-27 03:35:50 -0500

    added serialization to Action class

--- lib/Action.php
+++ lib/Action.php
@@ -15,6 +15,8 @@ use Closure;
 class Action
 {
     private ReflectionFunction $MethodInfo;
+    private array $StaticVariables = [];
+    private string $Syntax = '';
 
     public function __get(string $name)
     {
@@ -27,13 +29,47 @@ class Action
         $this->MethodInfo = new ReflectionFunction($action);
     }
 
+    public function invokeArgs(array $args = [])
+    {
+        return $this->MethodInfo->invokeArgs($args);
+    }
+
     public function __invoke(...$args)
     {
         return $this->invokeArgs($args);
     }
 
-    public function invokeArgs(array $args = [])
+    public function  __serialize(): array
     {
-        return $this->MethodInfo->invokeArgs($args);
+        $staticVariables = $this->MethodInfo->getStaticVariables();
+
+        $fileName = $this->MethodInfo->getFileName();
+        $startLine = $this->MethodInfo->getStartLine() - 1; // adjustment by - 1, because line 1 is in inedx 0
+        $endLine = $this->MethodInfo->getEndLine();
+        $length = $endLine - $startLine;
+
+        $source = file($fileName, FILE_IGNORE_NEW_LINES);
+        $lines = array_slice($source, $startLine, $length);
+        $syntax = implode(PHP_EOL, $lines);
+        preg_match("/(?i)function(.|\s)*\}/", $syntax, $matches);
+        if (!$matches[0]) {
+            throw new \Exception("Syntax Error, Closure can't be serialized");
+        }
+        $syntax = $matches[0] . ";";
+
+        return ['StaticVariables' => $staticVariables, 'Syntax' => $syntax];
+    }
+
+    public function __unserialize(array $data): void
+    {
+        $this->StaticVariables = $data['StaticVariables'];
+        $this->Syntax = $data['Syntax'];
+
+        extract($this->StaticVariables);
+        $closure = eval("return " . $this->Syntax);
+        $this->__construct($closure);
+        
+        $this->Syntax = '';
+        $this->StaticVariables = [];
     }
 }
