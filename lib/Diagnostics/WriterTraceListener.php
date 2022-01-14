e205ea6b33750540dc0851510b76a800e7211468
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-01-14 02:44:46 -0500

    create WriterTraceListener class

---
+++ lib/Diagnostics/WriterTraceListener.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\System\Diagnostics;
+
+class WriterTraceListener extends TraceListener
+{
+    protected bool $Indent;
+
+    public function write($value, ?string $category = null): void
+    {
+        $message = '';
+        $type = gettype($value);
+        switch ($type) {
+            case 'NULL':
+                $message = 'null';
+                break;
+            case 'boolean':
+                if ($type) {
+                    $message = 'true';
+                } else {
+                    $message = 'false';
+                }
+                break;
+            case 'integer':
+            case 'double':
+            case 'string':
+                $message = (string) $value;
+                break;
+            case 'array':
+                $message = 'array [' . count($value) . ']';
+                break;
+            case 'object':
+                $message = get_class($value);
+                break;
+            case 'resource':
+                $message = 'resource';
+                break;
+            default:
+                $message = 'unknown type';
+                break;
+        }
+
+        if ($this->NeedIndent && $this->IndentLevel) {
+            $spaces = '';
+            $indent = $this->IndentLevel * $this->IndentSize;
+            for ($i = 0; $i < $indent; $i++) {
+                $spaces .= ' ';
+            }
+
+            $message = $spaces . $message;
+        }
+
+        if ($category) {
+            $message = $category . ': ' . $message;
+        }
+
+        $this->Writer->write($message);
+        $this->NeedIndent = false;
+    }
+
+    public function writeLine($value, ?string $category = null): void
+    {
+        $this->write($value, $category);
+        $this->Writer->write(PHP_EOL);
+        $this->NeedIndent = true;
+    }
+
+    public function caller(int $skipFrames = 0): void
+    {
+        // adapt the frame level to the outer scope by one step.
+        $skipFrames++;
+        $stack  = new StackTrace($skipFrames);
+        $frame  = $stack->getFrame(0);
+        $file   = $frame->FileName . ':' . $frame->LineNumber;
+        $frame  = $stack->getFrame(1);
+        $caller = $frame->FunctionName . '()';
+        if ($frame->ClassName) {
+            $caller = $frame->ClassName . '::' . $caller;
+        }
+
+        $this->writeLine('at ' . $caller);
+        $this->writeLine('in ' . $file);
+    }
+}
