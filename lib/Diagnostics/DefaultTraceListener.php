c0363f74afed0c2eb79192d0a2145cb1dac64cf1
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-01-14 23:42:36 -0500

    added DefaultTraceListener

---
+++ lib/Diagnostics/DefaultTraceListener.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\System\Diagnostics;
+
+use DevNet\System\IO\FileStream;
+
+class DefaultTraceListener extends WriterTraceListener
+{
+    public function __construct()
+    {
+        $this->Writer = new FileStream('php://stdout', 'w');
+    }
+}
