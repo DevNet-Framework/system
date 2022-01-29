45b25cb9d6d7da4b1faec8d09f853d37507604ae
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-01-29 14:24:25 -0500

    update Debug class remove notice, warning methods rename error method to fail improve assert, fail, and log methods

--- lib/Diagnostics/Debug.php
+++ lib/Diagnostics/Debug.php
@@ -15,7 +15,7 @@ use DevNet\System\IO\ConsoleColor;
 
 class Debug extends Trace
 {
-    private int $FrameLevel = 0;
+    private int $FrameLevel = 1;
 
     public function _get(string $name)
     {
@@ -28,6 +28,21 @@ class Debug extends Trace
         $this->Listeners->add(new DefaultTraceListener());
     }
 
+    public function assert(bool $condition, string $message): void
+    {
+        if (!$condition) {
+            $this->FrameLevel++;
+            $this->fail($message);
+        }
+    }
+
+    public function fail(string $message): void
+    {
+        $this->FrameLevel++;
+        $this->log($message, 'Assertion Failed');
+        exit;
+    }
+
     public function log($value, ?string $category = null): void
     {
         $time = DateTime::createFromFormat('U.u', microtime(TRUE));
@@ -35,89 +50,7 @@ class Debug extends Trace
         $this->writeLine($value, $category);
         $this->indent();
         $this->write('at ');
-        $this->caller(1);
+        $this->caller($this->FrameLevel);
         $this->unindent();
-    }
-
-    public function notice(string $message): void
-    {
-        $report = error_reporting();
-        if (
-            $report == E_ALL ||
-            $report == E_NOTICE ||
-            $report == (E_NOTICE | E_WARNING) ||
-            $report == (E_NOTICE | E_ERROR)
-        ) {
-            Console::foregroundColor(ConsoleColor::Green);
-            $time = DateTime::createFromFormat('U.u', microtime(TRUE));
-            $this->write('[' . $time->format('H:i:s.v') . '] ');
-            $this->writeLine($message, 'Notice');
-            Console::foregroundColor(ConsoleColor::Green);
-            $this->indent();
-            $this->write('at ');
-            $this->caller(1);
-            $this->unindent();
-            Console::resetColor();
-        }
-    }
-
-    public function warning(string $message): void
-    {
-        $report = error_reporting();
-        if (
-            $report == E_ALL ||
-            $report == E_WARNING ||
-            $report == (E_WARNING | E_NOTICE) ||
-            $report == (E_WARNING | E_ERROR)
-        ) {
-            Console::foregroundColor(ConsoleColor::Yellow);
-            $time = DateTime::createFromFormat('U.u', microtime(TRUE));
-            $this->write('[' . $time->format('H:i:s.v') . '] ');
-            $this->writeLine($message, 'Wraning');
-            Console::foregroundColor(ConsoleColor::Yellow);
-            $this->indent();
-            $this->write('at ');
-            $this->caller(1);
-            $this->unindent();
-            Console::resetColor();
-        }
-    }
-
-    public function error(string $message): void
-    {
-        $report = error_reporting();
-        if (
-            $report == E_ALL ||
-            $report == E_ERROR ||
-            $report == (E_ERROR | E_NOTICE) ||
-            $report == (E_ERROR | E_WARNING)
-        ) {
-            Console::foregroundColor(ConsoleColor::Red);
-            $time = DateTime::createFromFormat('U.u', microtime(TRUE));
-            $this->write('[' . $time->format('H:i:s.v') . '] ');
-            $this->writeLine($message, 'Error');
-            Console::foregroundColor(ConsoleColor::Red);
-            $this->indent();
-            $this->write('at ');
-            $this->caller(1);
-            $this->unindent();
-            Console::resetColor();
-        }
-    }
-
-    public function assert(bool $condition, string $message): void
-    {
-        if (!$condition) {
-            Console::foregroundColor(ConsoleColor::Red);
-            $time = DateTime::createFromFormat('U.u', microtime(TRUE));
-            $this->write('[' . $time->format('H:i:s.v') . '] ');
-            $this->writeLine($message, 'Assertion Failed');
-            Console::foregroundColor(ConsoleColor::Red);
-            $this->indent();
-            $this->write('at ');
-            $this->caller(1);
-            $this->unindent();
-            Console::resetColor();
-        }
     }
 }
