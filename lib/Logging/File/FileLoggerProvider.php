42de8c2d6e1a8bdf1f38dc3436599274740885cf
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-02-01 13:56:22 -0500

    add FileLoggerProvider

---
+++ lib/Logging/File/FileLoggerProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\System\Logging\File;
+
+use DevNet\System\Logging\ILogger;
+use DevNet\System\Logging\ILoggerProvider;
+
+class FileLoggerProvider implements ILoggerProvider
+{
+    private string $FileName;
+
+    public function __construct(string $fileName)
+    {
+        $this->FileName = $fileName;
+    }
+
+    public function createLogger(string $category): ILogger
+    {
+        return new FileLogger($category, $this->FileName);
+    }
+}
