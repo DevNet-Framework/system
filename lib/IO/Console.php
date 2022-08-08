4612fdef9db2d06fff5fa98f6dda53866864ee12
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-08-08 00:45:53 -0400

    update Console class remove colors properties and use ConsoleColor instead

--- lib/IO/Console.php
+++ lib/IO/Console.php
@@ -11,36 +11,6 @@ namespace DevNet\System\IO;
 
 class Console
 {
-    private const FGCOLORS = [
-        0  => '0;30',
-        1  => '1;30',
-        2  => '0;31',
-        3  => '0;35',
-        4  => '0;34',
-        5  => '0;36',
-        6  => '0;32',
-        7  => '0;33',
-        8  => '0;37',
-        9  => '1;31',
-        10 => '1;35',
-        11 => '1;34',
-        12 => '1;36',
-        13 => '1;32',
-        14 => '1;33',
-        15 => '1;37'
-    ];
-
-    private const BGCOLORS = [
-        0  => '40',
-        2  => '41',
-        3  => '45',
-        4  => '44',
-        5  => '46',
-        6  => '42',
-        7  => '43',
-        15 => '47'
-    ];
-
     private static FileStream $In;
     private static FileStream $Out;
 
@@ -91,12 +61,12 @@ class Console
             self::write($value);
         }
 
-        return str_replace(PHP_EOL, '', self::$In->readLine());
+        return trim(self::$In->readLine());
     }
 
     public static function foregroundColor(int $color): void
     {
-        $color = self::FGCOLORS[$color] ?? null;
+        $color = ConsoleColor::parse('frontground', $color);
         if ($color !== null) {
             self::write("\e[${color}m");
         }
@@ -104,7 +74,7 @@ class Console
 
     public static function backgroundColor(int $color): void
     {
-        $color = self::BGCOLORS[$color] ?? null;
+        $color = ConsoleColor::parse('background', $color);
         if ($color !== null) {
             self::write("\e[${color}m");
         }
