d546710de38b93d0ef38df97c956a1d0b72dc5ec
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-08-22 22:29:38 -0400

    update ObjectTrait remove using traits implment _call() method for async and extension methods implment implement _get and _set for magic properties add setGenericType() method add getType() method add getHashCode() method

--- lib/ObjectTrait.php
+++ lib/ObjectTrait.php
@@ -9,69 +9,104 @@
 
 namespace DevNet\System;
 
-use DevNet\System\Async\AsyncTrait;
+use DevNet\System\Async\AsyncFunction;
+use DevNet\System\Exceptions\MethodException;
 use DevNet\System\Exceptions\PropertyException;
-use DevNet\System\Extension\ExtensionTrait;
-use DevNet\System\Reflection\ReflectionTrait;
 
 trait ObjectTrait
 {
-    use AsyncTrait, ExtensionTrait, ReflectionTrait {
-        AsyncTrait::__call as private callAsync;
-        ExtensionTrait::__call as private callExtension;
-    }
+    private ?Type $_type = null;
 
-    public function __call(string $name, array $args)
+    public function __get(string $property)
     {
-        try {
-            if (method_exists($this, 'async_' . $name)) {
-                return $this->callAsync($name, $args);
-            } else {
-                return $this->callExtension($name, $args);
-            }
-        } catch (\Throwable $error) {
-            throw $error;
-        }
-    }
-
-    public function __get(string $name)
-    {
-        $method = 'get_' . $name;
-        if (method_exists($this, $method)) {
-            $property = substr(strrchr($method, '_'), 1);
+        $accessor = 'get_' . $property;
+        if (method_exists($this, $accessor)) {
+            $accessor = $this->getType()->getMethod($accessor)->getName();
+            $name = substr(strrchr($accessor, '_'), 1);
             if ($name == $property) {
-                return $this->$method();
+                return $this->$accessor();
             }
         }
 
-        if (method_exists($this, $name)) {
-            return [$this, $name];
+        if (method_exists($this, $property)) {
+            return [$this, $property];
         }
 
         $class = get_class($this);
-        if (!property_exists($this, $name)) {
-            throw new PropertyException("Access to undefined property {$class}::{$name}");
+        if (!property_exists($this, $property)) {
+            throw new PropertyException("Access to undefined property {$class}::{$property}", 0, 1);
         }
 
-        throw new PropertyException("Access to non-public property {$class}::{$name}");
+        throw new PropertyException("Access to non-public property {$class}::{$property}", 0, 1);
     }
 
-    public function __set(string $name, $value): void
+    public function __set(string $property, $value): void
     {
-        $method = 'set_' . $name;
-        if (method_exists($this, $method)) {
-            $property = substr(strrchr($method, '_'), 1);
+        $accessor = 'set_' . $property;
+        if (method_exists($this, $accessor)) {
+            $accessor = $this->getType()->getMethod($accessor)->getName();
+            $name = substr(strrchr($accessor, '_'), 1);
             if ($name == $property) {
-                $this->$method($value);
+                $this->$accessor($value);
                 return;
             }
         }
 
         $class = get_class($this);
-        if (!property_exists($this, $name)) {
-            throw new PropertyException("Access to undefined property {$class}::{$name}");
+        if (!property_exists($this, $property)) {
+            throw new PropertyException("Access to undefined property {$class}::{$property}", 0, 1);
         }
 
-        throw new PropertyException("Access to non-public property {$class}::{$name}");
+        throw new PropertyException("Access to non-public property {$class}::{$property}", 0, 1);
+    }
+
+    public function __call(string $method, array $args)
+    {
+        $asyncMethod = 'async_' . $method;
+        if (method_exists($this, $asyncMethod)) {
+            $action = new AsyncFunction([$this, $asyncMethod]);
+            return $action->invokeArgs($args);
+        }
+
+        $extender = new Extender($this);
+        $extensionMethod = $extender->getMethod($method);
+        if ($extensionMethod) {
+            array_unshift($args, $this);
+            return $extensionMethod->invokeArgs(null, $args);
+        }
+
+        $class = get_class($this);
+        if (!method_exists($this, $method)) {
+            throw new MethodException("Call to undefined method {$class}::{$method}()", 0, 1);
+        }
+
+        throw new MethodException("Call to non-public method {$class}::{$method}()", 0, 1);
+    }
+
+    protected function setGenericType(array $typeArguments): void
+    {
+        if (!$this->_type) {
+            $this->_type = new Type(self::class, $typeArguments);
+        }
+    }
+
+    /**
+     * Get the type of the current object.
+     */
+    public function getType(): Type
+    {
+        if (!$this->_type) {
+            $this->_type = new Type(self::class);
+        }
+
+        return $this->_type;
+    }
+
+    /**
+     * Get a hash code id for the current object
+     */
+    public function getHashCode(): string
+    {
+        return spl_object_hash($this);
     }
 }
