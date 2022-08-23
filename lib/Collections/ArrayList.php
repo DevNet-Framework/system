a161eeb5b820d1b9765343706b33409395f11e4f
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-08-23 03:58:20 -0400

    update ArrayList remove use of ol traits and use only ArrayTrait add type checking to the method arguments

--- lib/Collections/ArrayList.php
+++ lib/Collections/ArrayList.php
@@ -9,55 +9,76 @@
 
 namespace DevNet\System\Collections;
 
-use DevNet\System\Exceptions\ArrayException;
+use DevNet\System\ArrayTrait;
+use DevNet\System\Exceptions\ArgumentException;
+use DevNet\System\Exceptions\TypeException;
+use DevNet\System\Type;
 use ArrayAccess;
 
 class ArrayList implements ArrayAccess, IList
 {
-    use \DevNet\System\Collections\ArrayTrait;
-    use \DevNet\System\Extension\ExtensionTrait;
-    use \DevNet\System\Reflection\ReflectionTrait;
+    use ArrayTrait;
 
     public function __construct(string $valueType)
     {
-        $this->Type = $this->getType()->makeGenericType(['int', $valueType]);
+        $this->setGenericType(['int', $valueType]);
     }
 
-    public function add($value): void
+    public function addRange(array $array): void
     {
-        $this->offsetSet(null, $value);
+        try {
+            foreach ($array as $value) {
+                $this->offsetSet(null, $value);
+            }
+        } catch (TypeException $exception) {
+            $genericArgs = $this->getType()->getGenericArguments();
+            throw new ArgumentException(self::class . "::addRange(): The argument #1, must be of type array<{$genericArgs[1]}>", 0, 1);
+        }
+    }
+
+    public function add($item): void
+    {
+        try {
+            $this->offsetSet(null, $item);
+        } catch (TypeException $exception) {
+            $genericArgs = $this->getType()->getGenericArguments();
+            throw new ArgumentException(self::class . "::add(): The argument #1, must be of type {$genericArgs[1]}", 0, 1);
+        }
     }
 
     public function contains($item): bool
     {
-        return in_array($item, $this->Array);
-    }
-
-    public function addRange(array $array)
-    {
-        foreach ($array as $key => $value) {
-            if (gettype($key) != "integer") {
-                throw ArrayException::invalidValueType("integer");
-            }
-
-            $this->offsetSet(null, $value);
+        $genericArgs = $this->getType()->getGenericArguments();
+        if (!Type::getType($item)->isEquivalentTo($genericArgs[1])) {
+            throw new ArgumentException(self::class . "::contains(): The argument #1, must be of type {$genericArgs[1]}", 0, 1);
         }
+
+        foreach ($this->getIterator() as $value) {
+            if ($value == $item) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public function remove($item): void
     {
-        if (isset($this->Array[$item])) {
-            unset($this->Array[$item]);
+        $genericArgs = $this->getType()->getGenericArguments();
+        if (!Type::getType($item)->isEquivalentTo($genericArgs[1])) {
+            throw new ArgumentException(self::class . "::remove(): The argument #1, must be of type {$genericArgs[1]}", 0, 1);
+        }
+
+        foreach ($this->getIterator() as $key => $value) {
+            if ($item == $value) {
+                $this->offsetUnset($key);
+                break;
+            }
         }
     }
 
-    public function clear(): void
+    public function removeAt(int $index): void
     {
-        $this->Array = [];
-    }
-
-    public function toArray(): array
-    {
-        return $this->Array;
+        $this->offsetUnset($index);
     }
 }
