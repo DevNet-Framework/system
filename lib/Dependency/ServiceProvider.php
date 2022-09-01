51f54f8ba73747bedca28d24f650a3879a00d02b
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-09-01 15:37:37 -0400

    update ServiceProvider throw new TypeException

--- lib/Dependency/ServiceProvider.php
+++ lib/Dependency/ServiceProvider.php
@@ -9,6 +9,8 @@
 
 namespace DevNet\System\Dependency;
 
+use DevNet\System\Exceptions\TypeException;
+
 class ServiceProvider implements IServiceProvider
 {
     private array $instanceServices = [];
@@ -70,7 +72,7 @@ class ServiceProvider implements IServiceProvider
                     $instance = $factory($this);
 
                     if (!$instance instanceof $serviceType) {
-                        throw new \Exception("Return value of factory function must be of the type '$serviceType'");
+                        throw new TypeException("Return value of factory function must be of the type '$serviceType'", 0, 1);
                     }
 
                     $this->instanceServices[$serviceType] = $instance;
