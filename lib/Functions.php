72a562ef47d6e5b156b1200a79aed0d514d259f4
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-04-05 20:52:50 -0400

    update function helpers add namespace to avoid naming conflict remove str_contains(), str_starts_with() and str_ends_with() methods

--- lib/Functions.php
+++ lib/Functions.php
@@ -7,34 +7,31 @@
  * @link        https://github.com/DevNet-Framework
  */
 
-use DevNet\System\Tasks\AsyncFunction;
-use DevNet\System\Diagnostics\Debug;
-use DevNet\System\Type;
+namespace Devnet\System {
 
-/**
- * add async helper.
- */
-if (!function_exists("async")) {
+    use DevNet\System\Diagnostics\Debug;
+    use DevNet\System\Async\AsyncFunction;
+    use DevNet\System\Type;
+
+    /**
+     * add async helper.
+     */
     function async(callable $action): AsyncFunction
     {
         return new AsyncFunction($action);
     }
-}
 
-/**
- * add typeOf helper.
- */
-if (!function_exists("typeOf")) {
+    /**
+     * add typeOf helper.
+     */
     function typeOf(string $typeName, array $typeArguments = []): Type
     {
         return new Type($typeName, $typeArguments);
     }
-}
 
-/**
- * add debug helper.
- */
-if (!function_exists("debug")) {
+    /**
+     * add debug helper.
+     */
     function debug($value = null): Debug
     {
         $debug = Debug::getInstance();
@@ -51,26 +48,49 @@ if (!function_exists("debug")) {
     }
 }
 
-/**
- * add some PHP 8 features to PHP 7.
- */
-if (!function_exists("str_contains")) {
-    function str_contains(string $string, string $needle): bool
-    {
-        return strpos($string, $needle) !== false ? true : false;
-    }
-}
+namespace {
 
-if (!function_exists("str_starts_with")) {
-    function str_starts_with(string $string, string $needle): bool
-    {
-        return strpos($string, $needle) === 0 ? true : false;
-    }
-}
+    use DevNet\System\Diagnostics\Debug;
+    use DevNet\System\Async\AsyncFunction;
+    use DevNet\System\Type;
 
-if (!function_exists("str_ends_with")) {
-    function str_ends_with(string $string, string $needle): bool
-    {
-        return strpos(strrev($string), strrev($needle)) === 0 ? true : false;
+    /**
+     * add async helper.
+     */
+    if (!function_exists("async")) {
+        function async(callable $action): AsyncFunction
+        {
+            return new AsyncFunction($action);
+        }
+    }
+
+    /**
+     * add typeOf helper.
+     */
+    if (!function_exists("typeOf")) {
+        function typeOf(string $typeName, array $typeArguments = []): Type
+        {
+            return new Type($typeName, $typeArguments);
+        }
+    }
+
+    /**
+     * add debug helper.
+     */
+    if (!function_exists("debug")) {
+        function debug($value = null): Debug
+        {
+            $debug = Debug::getInstance();
+            if (func_num_args()) {
+                $time = \DateTime::createFromFormat('U.u', microtime(TRUE));
+                $debug->write('[' . $time->format('H:i:s.v') . '] ');
+                $debug->writeLine($value, 'Debug');
+                $debug->indent();
+                $debug->write('at ');
+                $debug->caller(1);
+                $debug->unindent();
+            }
+            return $debug;
+        }
     }
 }
