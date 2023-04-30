58d301f521ee61aad8386b360f3dc45e6a6d2084
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-04-30 02:05:23 -0400

    change ConsoleColor change ConsoleColor class to enum

--- lib/IO/ConsoleColor.php
+++ lib/IO/ConsoleColor.php
@@ -9,93 +9,68 @@
 
 namespace DevNet\System\IO;
 
-class ConsoleColor
+use DevNet\System\Exceptions\ArgumentException;
+
+enum ConsoleColor: int
 {
-    public const Black       = 0;
-    public const DarkGrey    = 1;
-    public const DarkRed     = 2;
-    public const DarkMagenta = 3;
-    public const DarkBlue    = 4;
-    public const DarkCyan    = 5;
-    public const DarkGreen   = 6;
-    public const DarkYellow  = 7;
-    public const Gray        = 8;
-    public const Red         = 9;
-    public const Magenta     = 10;
-    public const Blue        = 11;
-    public const Cyan        = 12;
-    public const Green       = 13;
-    public const Yellow      = 14;
-    public const White       = 15;
+    case Black       = 0;
+    case DarkGrey    = 1;
+    case DarkRed     = 2;
+    case DarkMagenta = 3;
+    case DarkBlue    = 4;
+    case DarkCyan    = 5;
+    case DarkGreen   = 6;
+    case DarkYellow  = 7;
+    case Gray        = 8;
+    case Red         = 9;
+    case Magenta     = 10;
+    case Blue        = 11;
+    case Cyan        = 12;
+    case Green       = 13;
+    case Yellow      = 14;
+    case White       = 15;
 
-    private const frontground = [
-        0  => '0;30',
-        1  => '1;30',
-        2  => '0;31',
-        3  => '0;35',
-        4  => '0;34',
-        5  => '0;36',
-        6  => '0;32',
-        7  => '0;33',
-        8  => '0;37',
-        9  => '1;31',
-        10 => '1;35',
-        11 => '1;34',
-        12 => '1;36',
-        13 => '1;32',
-        14 => '1;33',
-        15 => '1;37'
-    ];
-
-    private const background = [
-        8   => '40',
-        9   => '41',
-        10  => '45',
-        11  => '44',
-        12  => '46',
-        13  => '42',
-        14  => '43',
-        15  => '47'
-    ];
-
-    public static function getNames(): array
-    {
-        return [
-            'Black',
-            'DarkGrey',
-            'DarkRed',
-            'DarkMagenta',
-            'DarkBlue',
-            'DarkCyan',
-            'DarkGreen',
-            'DarkYellow',
-            'Gray',
-            'Red',
-            'Magenta',
-            'Blue',
-            'Cyan',
-            'Green',
-            'Yellow',
-            'White'
-        ];
-    }
-
-    public static function getValues(): array
-    {
-        return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    }
-
-    public static function parse(string $type, int $value): ?string
+    /**
+     * @param int $type Color type must be 1 for frontground or 2 for background.
+     * @throws ArgumentException If the argument is not equal to the value 1 or 2.
+     */
+    public function parse(int $type): ?string
     {
         switch ($type) {
-            case 'frontground':
-                return self::frontground[$value] ?? null;
+            case 1:
+                return match ($this) {
+                    self::Black       => '0;30',
+                    self::DarkGrey    => '1;30',
+                    self::DarkRed     => '0;31',
+                    self::DarkMagenta => '0;35',
+                    self::DarkBlue    => '0;34',
+                    self::DarkCyan    => '0;36',
+                    self::DarkGreen   => '0;32',
+                    self::DarkYellow  => '0;33',
+                    self::Gray        => '0;37',
+                    self::Red         => '1;31',
+                    self::Magenta     => '1;35',
+                    self::Blue        => '1;34',
+                    self::Cyan        => '1;36',
+                    self::Green       => '1;32',
+                    self::Yellow      => '1;33',
+                    self::White       => '1;37'
+                };
                 break;
-            case 'background':
-                return self::background[$value] ?? null;
+            case 2:
+                return match ($this) {
+                    self::Gray    => '40',
+                    self::Red     => '41',
+                    self::Magenta => '45',
+                    self::Blue    => '44',
+                    self::Cyan    => '46',
+                    self::Green   => '42',
+                    self::Yellow  => '43',
+                    self::White   => '47'
+                };
                 break;
             default:
-                return null;
+                throw new ArgumentException("Color type must be 1 for frontground or 2 for background!", 0, 1);
                 break;
         }
     }
