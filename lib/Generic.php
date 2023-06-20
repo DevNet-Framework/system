b3a0b5870200aa31e3c1ad02ebcd7c42dc343f11
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-06-20 16:09:44 -0400

    create Generic annotation

---
+++ lib/Generic.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\System;
+
+use Attribute;
+
+#[Attribute]
+class Generic
+{
+    private array $types = [];
+
+    public function __construct(string ...$typeNames)
+    {
+        foreach ($typeNames as $typeName) {
+            $this->types[] = new Type($typeName);
+        }
+    }
+
+    public function getTypes(): array
+    {
+        return $this->types;
+    }
+}
