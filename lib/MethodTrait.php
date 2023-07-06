154b1bf505d410f43a8eddcd0382d7b9143401f0
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-07-06 16:07:57 -0400

    update MethodTrait - add case of invoking method with generic params

--- lib/MethodTrait.php
+++ lib/MethodTrait.php
@@ -11,6 +11,7 @@ namespace DevNet\System;
 
 use DevNet\System\Async\AsyncFunction;
 use DevNet\System\Async\Task;
+use DevNet\System\Exceptions\ArgumentException;
 use DevNet\System\Exceptions\MethodException;
 use ReflectionMethod;
 
@@ -18,30 +19,58 @@ trait MethodTrait
 {
     public function __call(string $method, array $args)
     {
-        $asyncMethod = 'async_' . $method;
-        if (method_exists($this, $asyncMethod)) {
-            $action = new AsyncFunction([$this, $asyncMethod]);
-            return $action->invoke($args);
-        }
-
-        $provider = new MethodProvider($this);
-        $extensionMethod = $provider->getMethod($method);
-        if ($extensionMethod) {
-            array_unshift($args, $this);
-            return $extensionMethod->invokeArgs(null, $args);
-        }
-
-        if (method_exists($this, $method)) {
-            $modifier = 'private';
-            $methodInfo = new ReflectionMethod($this, $method);
-            if ($methodInfo->isProtected()) {
-                $modifier = 'protected';
+        if (!method_exists($this, $method)) {
+            $asyncMethod = 'async_' . $method;
+            if (method_exists($this, $asyncMethod)) {
+                $action = new AsyncFunction([$this, $asyncMethod]);
+                return $action->invoke($args);
             }
 
-            throw new MethodException("Call to {$modifier} method " . static::class . "::{$method}()", 0, 1);
-        }
+            $provider = new MethodProvider($this);
+            $extensionMethod = $provider->getMethod($method);
+            if ($extensionMethod) {
+                array_unshift($args, $this);
+                return $extensionMethod->invokeArgs(null, $args);
+            }
 
-        throw new MethodException("Call to undefined method "  . static::class . "::{$method}()", 0, 1);
+            throw new MethodException("Call to undefined method "  . static::class . "::{$method}()", 0, 1);
+        } else {
+            $hasGenericParameter = false;
+            $genericArgs = $this->getType()->getGenericArguments();
+            if ($genericArgs) {
+                $methodInfo = new ReflectionMethod($this, $method);
+                foreach ($methodInfo->getParameters() as $index => $param) {
+                    if ($param->hasType()) {
+                        $paramTypeName = $param->getType()->getName();
+                        $genericArgument = $genericArgs[$paramTypeName] ?? null;
+                        if ($genericArgument) {
+                            $hasGenericParameter = true;
+                            $argument = $args[$index];
+                            $typeArgument = Type::getType($argument);
+                            if (!$typeArgument->isAssignableTo($genericArgument)) {
+                                $index++;
+                                throw new ArgumentException("Argument #{$index} must be of type {$genericArgument}", 0, 1);
+                            }
+                            $element = new $paramTypeName($typeArgument);
+                            $element->Value = $args[$index];
+                            $args[$index] = $element;
+                        }
+                    }
+                }
+
+                if ($hasGenericParameter) {
+                    $methodInfo->setAccessible(true);
+                    return $methodInfo->invokeArgs($this, $args);
+                }
+
+                $modifier = 'private';
+                if ($methodInfo->isProtected()) {
+                    $modifier = 'protected';
+                }
+
+                throw new MethodException("Call to {$modifier} method " . static::class . "::{$method}()", 0, 1);
+            }
+        }
     }
 
     public function __invoke(...$args): Task
