31770950966ca7efa619621b91d0cd0cbf793185
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-09-27 22:28:47 -0400

    update Dictionary collection - remove K, V classes and use generic parameters K, V instead - use TypeException instead of ArgumentException - fix TypeExcpetion messages

--- lib/Collections/Dictionary.php
+++ lib/Collections/Dictionary.php
@@ -9,71 +9,44 @@
 
 namespace DevNet\System\Collections;
 
-use DevNet\System\Exceptions\ArgumentException;
+use DevNet\System\Exceptions\TypeException;
 use DevNet\System\Template;
 use DevNet\System\Type;
 
-class K extends \DevNet\System\Parameter {}
-class V extends \DevNet\System\Parameter {}
-
-#[Template(K::class, V::class)]
+#[Template('K', 'V')]
 class Dictionary extends AbstractArray implements IDictionary
 {
     public function __construct(string $keyType, string $valueType)
     {
         $keyType = strtolower($keyType);
         if ($keyType != 'int' && $keyType != 'integer' &&  $keyType != 'string') {
-            throw new ArgumentException(self::class . "::__construct(): Key type must be defined as integer or string", 0, 1);
+            throw new TypeException(static::class . "::__construct(): Key type must be defined as integer or string", 0, 1);
         }
 
         $this->setGenericArguments($keyType, $valueType);
     }
 
-    public function add($key, $value): void
+    public function add(#[Type('K')] $key, #[Type('V')] $value): void
     {
-        $genericArgs = $this->getType()->getGenericArguments();
-
-        if (!Type::getType($key)->isEquivalentTo($genericArgs[0])) {
-            throw new ArgumentException(self::class . "::add(): The argument #1, must be of type {$genericArgs[0]}", 0, 1);
-        }
-
-        if (!Type::getType($value)->isEquivalentTo($genericArgs[1])) {
-            throw new ArgumentException(self::class . "::add(): The argument #2, must be of type {$genericArgs[1]}", 0, 1);
-        }
-
+        $this->checkArgumentTypes(func_get_args());
         $this->array[$key] = $value;
     }
 
-    public function contains($key): bool
+    public function contains(#[Type('K')] $key): bool
     {
-        $genericArgs = $this->getType()->getGenericArguments();
-
-        if (!Type::getType($key)->isEquivalentTo($genericArgs[0])) {
-            throw new ArgumentException(self::class . "::contains(): The argument #1, must be of type {$genericArgs[0]}", 0, 1);
-        }
-
+        $this->checkArgumentTypes(func_get_args());
         return isset($this->array[$key]) ? true : false;
     }
 
-    public function remove($key): void
+    public function remove(#[Type('K')] $key): void
     {
-        $genericArgs = $this->getType()->getGenericArguments();
-
-        if (!Type::getType($key)->isEquivalentTo($genericArgs[0])) {
-            throw new ArgumentException(self::class . "::remove(): The argument #1, must be of type {$genericArgs[0]}", 0, 1);
-        }
-
+        $this->checkArgumentTypes(func_get_args());
         if (isset($this->array[$key])) unset($this->array[$key]);
     }
 
-    public function getValue($key)
+    public function getValue(#[Type('K')] $key)
     {
-        $genericArgs = $this->getType()->getGenericArguments();
-
-        if (!Type::getType($key)->isEquivalentTo($genericArgs[0])) {
-            throw new ArgumentException(self::class . "::getValue(): The argument #1, must be of type {$genericArgs[0]}", 0, 1);
-        }
-
+        $this->checkArgumentTypes(func_get_args());
         return $this->array[$key] ?? null;
     }
 }
