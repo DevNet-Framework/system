c2568d8a19c0a12458dacbcc1b9a3e97dc9bd3f2
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-11-15 05:37:26 -0500

    improve ClassLoader to map namespaces and files - add mapNamespace() method - add include() method

--- lib/Runtime/ClassLoader.php
+++ lib/Runtime/ClassLoader.php
@@ -14,28 +14,43 @@ use DirectoryIterator;
 class ClassLoader
 {
     private string $root;
-    private array $codeFiles = [];
+    private array $classes  = [];
+    private array $prefixes = [];
+    private array $files    = [];
 
     public function __construct(string $root)
     {
         $this->root = $root;
     }
 
-    public function map(string $path): void
+    public function map(string $directory, bool $allDirectories = true): void
     {
-        $this->codeFiles += $this->scanForCodeFiles($this->root . $path);
+        $this->classes += $this->scanForCodeFiles($this->root . $directory, $allDirectories);
     }
 
-    public function scanForCodeFiles(string $directory): array
+    function mapNamespace(string $prefix, string $directory): void
+    {
+        $this->prefixes[trim($prefix, '\\')] = $directory;
+    }
+
+    function include(string $path): void
+    {
+        if (is_file($path) && strtolower(pathinfo($path, PATHINFO_EXTENSION)) == 'php') {
+            $this->files[] = $path;
+        }
+    }
+
+    public function scanForCodeFiles(string $directory, bool $allDirectories): array
     {
         $files = [];
         foreach (new DirectoryIterator($directory) as $path) {
             if ($path->isDir() && !str_starts_with($path->getFilename(), '.')) {
-                $result = $this->scanForCodeFiles($directory . '/' . $path->getFilename());
-                $files = array_merge($files, $result);
+                if ($allDirectories) {
+                    $files += $this->scanForCodeFiles($directory . '/' . $path->getFilename(), $allDirectories);
+                }
             } else if ($path->isFile() && strtolower($path->getExtension()) == 'php') {
                 $content = file_get_contents($path->getRealPath());
-                preg_match_all('%namespace\s+([a-z][a-z0-9_\\\]*)\s*;|class\s+([a-z][a-z0-9_]*)|/\*(.|\n)*?\*/|//.*%i', $content, $matches);
+                preg_match_all('%namespace\s+([a-z][a-z0-9_\\\]*)\s*;|(class|interface|trait|enum)\s+([a-z][a-z0-9_]*)|/\*(.|\n)*?\*/|//.*%i', $content, $matches);
 
                 $namespace = null;
                 $namespaces = $matches[1] ?? [];
@@ -46,10 +61,10 @@ class ClassLoader
                     }
                 }
 
-                $classes = $matches[2] ?? [];
-                foreach ($classes as $class) {
-                    if ($class) {
-                        $files[$namespace . $class] = $path->getRealPath();
+                $names = $matches[3] ?? [];
+                foreach ($names as $name) {
+                    if ($name) {
+                        $files[$namespace . $name] = $path->getRealPath();
                     }
                 }
             }
@@ -60,15 +75,41 @@ class ClassLoader
 
     public function load(string $class): void
     {
-        $file = $this->codeFiles[$class] ?? null;
-        if ($file) {
-            @include_once $file;
+        $path = $this->classes[$class] ?? null;
+        if ($path) {
+            @include_once $path;
+            return;
+        }
+
+        $segments  = explode("\\", $class);
+        $className = array_pop($segments);
+        $namespace = implode("\\", $segments);
+
+        foreach ($this->prefixes as $prefix => $baseDirectory) {
+            $position = strpos($namespace, $prefix);
+
+            if ($position !== false) {
+                $subDirectory = substr_replace($namespace, "", $position, strlen($prefix));
+                $subDirectory = str_replace("\\", "/", $subDirectory);
+                $subDirectory = trim($subDirectory, "/");
+                $path         = $this->root . '/' . $baseDirectory . "/" . $subDirectory . "/" . $className . ".php";
+
+                if (is_file($path)) {
+                    @include_once $path;
+                    return;
+                }
+            }
         }
     }
 
     public function register(): void
     {
         spl_autoload_register([$this, 'load']);
+
+        // The code files must be loaded after the auto-loaded classes, because they rely on them.
+        foreach ($this->files as $file) {
+            @include_once $file;
+        }
     }
 
     public function unRegister(): void
