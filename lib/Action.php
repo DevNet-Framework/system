ff2e61f27114c287c9655a302ce639c7ac7343d0
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2023-11-18 22:54:01 -0500

    modify Action Delegate to represent any function

--- lib/Action.php
+++ lib/Action.php
@@ -9,11 +9,52 @@
 
 namespace DevNet\System;
 
-use DevNet\System\Delegate;
+use Closure;
+use ReflectionFunction;
+use ReflectionFunctionAbstract;
+use ReflectionMethod;
 
-class Action extends Delegate
+class Action
 {
-    public function action()
+    use PropertyTrait;
+
+    public ReflectionFunctionAbstract $reflection;
+    public $target;
+
+    public function __construct(callable $target)
     {
+        $this->target = $target;
+        if (is_array($target)) {
+            $this->reflection = new ReflectionMethod($target[0], $target[1]);
+        } else if (is_object($target)) {
+            if ($target instanceof Closure) {
+                $this->reflection = new ReflectionFunction($target);
+            } else {
+                $this->reflection = new ReflectionMethod($target, '__invoke');
+                $this->target = [$target, '__invoke'];
+            }
+        }
+    }
+
+    public function get_Reflection(): ReflectionFunctionAbstract
+    {
+        return $this->reflection;
+    }
+
+    public function invoke(...$args)
+    {
+        if ($this->reflection->isClosure()) {
+            $function = $this->target;
+            return $function(...$args);
+        }
+
+        $object = $this->target[0];
+        $method = $this->target[1];
+        return $object->$method(...$args);
+    }
+
+    public function __invoke(...$args)
+    {
+        $this->invoke(...$args);
     }
 }
